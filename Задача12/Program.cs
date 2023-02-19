491ce8077ca92bd840df800ae8ccc9df01b5eed1
Anastasia <sadness1995@yandex.ru>
2023-02-19 18:06:55 +0300

    Семинар 2

---
+++ Задача12/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то 
+// программа выводит остаток от деления.
+// ● 34, 5 -> не кратно, остаток 4
+// ● 16, 4 -> кратно
+
+Console.WriteLine("Введите первое число: ");
+int numberA =int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int numberB =int.Parse(Console.ReadLine()!);
+
+int numberC = numberA % numberB;
+
+if(numberA % numberB == 0)
+{
+  Console.WriteLine("второе число кратно первому");
+}
+else
+{
+  Console.WriteLine("Второе число не кратно первому, остаток " + numberC);
+}
---
+++ Задача16/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// ● 5, 25 -> да
+// ● -4, 16 -> да
+// ● 25, 5 -> да
+// ● 8,9 -> нет 
+
+Console.WriteLine("Введите первое число: ");
+int numberA =int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int numberB =int.Parse(Console.ReadLine()!);
+
+if(numberA * numberA == numberB || numberB * numberB == numberA)
+{
+  Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
