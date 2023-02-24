8f28d49efc3630706acd172f82f388602bd21517
Anastasia <sadness1995@yandex.ru>
2023-02-24 14:07:22 +0300

    Семинар 3

---
+++ Задача18/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти (1,2,3 или 4): ");
+int n = int.Parse(Console.ReadLine()!);
+
+if(n == 1)
+{
+  Console.WriteLine("x > 0, y > 0");
+}
+if(n == 2)
+{
+  Console.WriteLine("x < 0, y > 0");
+}
+if(n == 3)
+{
+  Console.WriteLine("x < 0, y < 0");
+}
+if(n == 4)
+{
+  Console.WriteLine("x > 0, y < 0");
+}
---
+++ Задача21/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+// Math.Sqrt() - корень
+
+// AB = √(xb - xa)2 + (yb - ya)2
+
+Console.Write("Введите координаты точки А: x = ");
+int X1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты точки А: y = ");
+int Y1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите координаты точки В: x = ");
+int X2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты точки B: y = ");
+int Y2 = int.Parse(Console.ReadLine()!);
+
+
+int X = (X2 - X1)*(X2 - X1);
+int Y = (Y2 - Y1)*(Y2 - Y1);
+int n = X + Y;
+
+double AB = Math.Sqrt(n);
+
+Console.Write(Math.Round (AB, 2));        //Math.Round - округление после запятой до указанного количества цифр, чтоб убрать длинный хвост
